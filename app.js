244031788e2c2179bf75ba3414bd2b1ba93dfef0
Paul Yabsley <yabsleyp@gmail.com>
2017-02-11 15:13:36 +0000

    Add editing of invitees

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const ul = document.getElementById('invitedList');
 // Creating new list items
 createLi = function(text) {
 	const li = document.createElement('li');
-	li.textContent = text;
+	const span = document.createElement('span');
+	span.textContent = text;
+	li.appendChild(span);
 	const label = document.createElement('label');
 	label.textContent = 'Confirmed';
 	const checkbox = document.createElement('input');
@@ -48,10 +50,24 @@ ul.addEventListener('click', (e) => {
 		const button = e.target;
 		const li = button.parentNode;
 		const ul = li.parentNode;
+
 		if (button.textContent === 'Remove') {
 			ul.removeChild(li);
 		} else if (button.textContent === 'Edit') {
-			console.log('edit');
+			const span = li.firstElementChild;
+			const input = document.createElement('input');
+			input.type = 'text';
+			input.value = span.textContent;
+			li.insertBefore(input, span);
+			li.removeChild(span);
+			button.textContent = 'Save';
+		} else if (button.textContent === 'Save') {
+			const input = li.firstElementChild;
+			const span = document.createElement('span');
+			span.textContent = input.value;
+			li.insertBefore(span, input);
+			li.removeChild(input);
+			button.textContent = 'Edit';
 		}
 	}
 });

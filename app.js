978f0378c192ccebf71ff8300e1522951a82db62
Paul Yabsley <yabsleyp@gmail.com>
2017-02-11 13:26:32 +0000

    Add event handler for adding invites

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('Hello from app.js');
+const form = document.getElementById('registrar');
+const input = form.querySelector('input');
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	let text = input.value;
+	input.value = '';
+	const ul = document.getElementById('invitedList');
+	const li = document.createElement('li');
+	li.textContent = text;
+	ul.appendChild(li);
+});
